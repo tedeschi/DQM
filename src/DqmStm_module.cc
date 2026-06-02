6d78427f68988bed2644855ccde3796cfe33cc03
Bryan Gonzalez <bgonzale@mu2ebuild02.fnal.gov>
2026-06-02 16:51:02 -0500

    Added STM Fragment Summaries to DQM and Unpacking

--- src/DqmStm_module.cc
+++ src/DqmStm_module.cc
@@ -10,10 +10,12 @@
 #include "art_root_io/TFileService.h"
 
 #include "Offline/RecoDataProducts/inc/STMWaveformDigi.hh"
+#include "Offline/RecoDataProducts/inc/STMFragmentSummary.hh"
 #include "Offline/DataProducts/inc/STMChannel.hh" 
 #include "Offline/Mu2eUtilities/inc/STMUtils.hh"
 #include "Offline/ProditionsService/inc/ProditionsHandle.hh"
 #include "Offline/RecoDataProducts/inc/STMPHDigi.hh"
+#include "Offline/STMConditions/inc/STMEnergyCalib.hh"
 
 #include "TH1F.h"
 
@@ -27,10 +29,14 @@ class DqmStm : public art::EDAnalyzer {
   struct Config {
     using Name = fhicl::Name;
     using Comment = fhicl::Comment;
-    //fhicl::Atom<art::InputTag> stmwaveformDigisTag{Name("stmWaveformDigisTag"), Comment("InputTag for STMWaveformDigiCollection")};
+    //For now using explciit tags
+    fhicl::Atom<art::InputTag> rawHPGeTag{Name("rawHPGeTag"), Comment("StmWaveform rawHPGe Collection"), art::InputTag()};
+    fhicl::Atom<art::InputTag> rawLaBrTag{Name("rawLaBrTag"), Comment("StmWaveform rawLaBr Collection"), art::InputTag()};
+    fhicl::Atom<art::InputTag> zsHPGeTag{Name("zsHPGeTag"), Comment("StmWaveform zsHPGe Collection"), art::InputTag()}; 
+    fhicl::Atom<art::InputTag> zsLaBrTag{Name("zsLaBrTag"), Comment("StmWaveform zsLaBr Collection"), art::InputTag()};
     fhicl::Atom<art::InputTag> phHPGeTag{Name("phHPGeTag"), Comment("StmDigi phHPge Collection"), art::InputTag()};
     fhicl::Atom<art::InputTag> phLaBrTag{Name("phLaBrTag"), Comment("StmDigi phLaBr Collection"), art::InputTag()};
-
+    fhicl::Atom<art::InputTag> fragSummaryTag{Name("fragSummaryTag"), Comment("STM fragment summary collection"), art::InputTag()};
   };
   typedef art::EDAnalyzer::Table<Config> Parameters;
 
@@ -44,25 +50,36 @@ class DqmStm : public art::EDAnalyzer {
 private:
   Config _conf;
 
+  //Version number tracked
   TH1D* _hVer;
+  // Counts
+  TH1D* _hNContainerFrags;
+  TH1D* _hNInnerFrags;
+  
+  TH1D* _hNRAWHPGe; //Raw Waveform size
+  TH1D* _hNRAWLaBr;
+
+  TH1D* _hNZSHPGe; //ZS Waveform size 
+  TH1D* _hNZSLaBr;                                               
 
   // PH digis
   TH1D* _hNPHHPGe; //Number of pulse heights for HPGe
   TH1D* _hPulseHeightHPGe; //Pulse Heigt of HPGe
-
-  TH1D* _hNPHLaBr; //Number of pulse heights for LaBr                                                                        
+  TH1D* _hNPHLaBr; //Number of pulse heights for LaBr                                                          
   TH1D* _hPulseHeightLaBr; //Pulse Heigt of LaBr
-  
-  // For later
-  //Th1D* _hRawCount;
-  //TH1D* _hZSCount;
   
 };
 /*****************************/
-  // configrues fcl to local variables  
+  // configrues fcl to local variables
+  //Will come back to simplify this with channel name
 DqmStm::DqmStm(const Parameters& conf) : art::EDAnalyzer(conf), _conf(conf()) {
   mayConsume<STMPHDigiCollection>(_conf.phHPGeTag()); //reads PH from the art file given in fcl
   mayConsume<STMPHDigiCollection>(_conf.phLaBrTag());
+  mayConsume<STMWaveformDigiCollection>(_conf.rawHPGeTag());
+  mayConsume<STMWaveformDigiCollection>(_conf.rawLaBrTag());
+  mayConsume<STMWaveformDigiCollection>(_conf.zsHPGeTag());
+  mayConsume<STMWaveformDigiCollection>(_conf.zsLaBrTag());
+  mayConsume<STMFragmentSummaryCollection>(_conf.fragSummaryTag());
   //Will add more in time such as raw, zs, frag count etc. 
 }
 
@@ -72,15 +89,36 @@ void DqmStm::beginJob() {
   art::ServiceHandle<art::TFileService> tfs;
   
   _hVer = tfs->make<TH1D>("Ver", "Version Number", 101, -0.5, 100.00);
-  
+  //histograms
+  if (!_conf.rawHPGeTag().empty()){
+    _hNRAWHPGe = tfs->make<TH1D>("NRawWaveformDigisHPGe", "N RAW HPGe WaveformDigis;Number of Raw WF Digis;Entries", 250, -0.5, 500.5);
+  }
+
+  if (!_conf.rawLaBrTag().empty()){
+    _hNRAWLaBr = tfs->make<TH1D>("NRawWaveformDigisLaBr", "N RAW LaBr WaveformDigis;Number of Raw WF Digis:Entries", 250, -0.5, 500.5);
+  }
+
+  if (!_conf.zsHPGeTag().empty()){
+    _hNZSHPGe = tfs->make<TH1D>("NZSWaveformDigisHPGe", "N ZS HPGe WaveformDigis;Number of ZS WF Digis;Entries", 250, -0.5, 500.5);
+  }
+
+  if (!_conf.zsLaBrTag().empty()){
+    _hNZSLaBr = tfs->make<TH1D>("NZSWaveformDigisLaBr", "N ZS LaBr WaveformDigis;Number of ZS WF Digis;Entries", 250, -0.5, 500.5);
+  }
+
   if (!_conf.phHPGeTag().empty()){
-    _hNPHHPGe = tfs->make<TH1D>("NPHDigisHPGe", "N PH HPGe Digis", 101, -0.5, 100.5);
-    _hPulseHeightHPGe = tfs->make<TH1D>("PulseHeightHPGe", "STM HPGe Pulse Height", 1000, 0.0, 10000.0);
+    _hNPHHPGe = tfs->make<TH1D>("NPHDigisHPGe", "N PH HPGe Digis;Number of Pulse Heiht Digis: Entries", 20, -0.5, 30.5);
+    _hPulseHeightHPGe = tfs->make<TH1D>("PulseHeightHPGe", "STM HPGe Pulse Height;Pulse Height;Entries", 1000, 0.0, 10000.0);
   }
 
   if (!_conf.phLaBrTag().empty()){
-    _hNPHLaBr = tfs->make<TH1D>("NPHDigisLaBr", "N PH LaBr Digis", 101, -0.5, 100.5);
-    _hPulseHeightLaBr = tfs->make<TH1D>("PulseHeightLaBr", "STM LaBr Pulse Height", 1000, 0.0, 10000.0);
+    _hNPHLaBr = tfs->make<TH1D>("NPHDigisLaBr", "N PH LaBr Digis;Number of Pulse Height Digis;Entries", 20, -0.5, 30.5);
+    _hPulseHeightLaBr = tfs->make<TH1D>("PulseHeightLaBr", "STM LaBr Pulse Height;Pulse Height;Entries", 1000, 0.0, 10000.0);
+  }
+
+  if (!_conf.fragSummaryTag().empty()){
+    _hNContainerFrags = tfs->make<TH1D>("NContainerFrags","N STM Container Fragments;Number of Container Frags;Events",20,-0.5,19.5);
+    _hNInnerFrags = tfs->make<TH1D>("NInnerFrags", "N STM Inner Fragments;Numver of Inner Frags;Events",200,-0.5,2000.5);
   }
   
 }
@@ -90,18 +128,50 @@ void DqmStm::beginJob() {
 void DqmStm::analyze(const art::Event& event) {
   _hVer->Fill(0.0);
 
+  if(!_conf.fragSummaryTag().empty()){
+    auto summaryHandle = event.getValidHandle<STMFragmentSummaryCollection>(_conf.fragSummaryTag());
+    for (const auto& summary : *summaryHandle){
+      _hNContainerFrags->Fill(summary.nContainerFrags());
+      _hNInnerFrags->Fill(summary.nInnerFrags());
+    }
+  }
+  
+  if (!_conf.rawHPGeTag().empty()) {
+    auto rawDigiHandle = event.getValidHandle<STMWaveformDigiCollection>(_conf.rawHPGeTag());
+    const auto& rawwaveform = *rawDigiHandle;
+    _hNRAWHPGe->Fill(rawwaveform.size());
+  }
+
+  if (!_conf.rawLaBrTag().empty()) {
+    auto rawDigiHandle = event.getValidHandle<STMWaveformDigiCollection>(_conf.rawLaBrTag());
+    const auto& rawwaveform = *rawDigiHandle;
+    _hNRAWLaBr->Fill(rawwaveform.size());
+  }
+
+
+  if (!_conf.zsHPGeTag().empty()) {
+    auto zsDigiHandle = event.getValidHandle<STMWaveformDigiCollection>(_conf.zsHPGeTag());
+    const auto& zswaveform = *zsDigiHandle;
+    _hNZSHPGe->Fill(zswaveform.size());
+  }
+
+  if (!_conf.zsLaBrTag().empty()) {
+    auto zsDigiHandle = event.getValidHandle<STMWaveformDigiCollection>(_conf.zsLaBrTag());
+    const auto& zswaveform = *zsDigiHandle;
+    _hNZSLaBr->Fill(zswaveform.size());
+  }
+
+
   if (!_conf.phHPGeTag().empty()) {
     auto phDigiHandle = event.getValidHandle<STMPHDigiCollection>(_conf.phHPGeTag());
     const auto& phDigis = *phDigiHandle;
-    
+
     _hNPHHPGe->Fill(phDigis.size());
-    
-    for (const auto& phDigi : phDigis) {
+
+    for (const auto& phDigi: phDigis) {
       _hPulseHeightHPGe->Fill(phDigi.energy());
     }
-    
   }
-  
 
   if (!_conf.phLaBrTag().empty()) {
     auto phDigiHandle = event.getValidHandle<STMPHDigiCollection>(_conf.phLaBrTag());
@@ -112,7 +182,6 @@ void DqmStm::analyze(const art::Event& event) {
     for (const auto& phDigi: phDigis) {
       _hPulseHeightLaBr->Fill(phDigi.energy());
     }
-    
   }
   
   
